8992651d862ac4183ece441c8aaff42dbaa0dfbc
JuanDavid38 <jdavid3ramirez.999@gmail.com>
2024-02-21 23:04:13 -0500

    envio archivos actualizados a main

--- JS/main.js
+++ JS/main.js
@@ -10,48 +10,86 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-$("#formulario_crear_foro").hide();
     // seccion docentes
+$("#formulario_registro_foros_docente").hide();
 $("#crear_foro").on("click", function() {
-    $("#formulario_crear_foro").show();
-    $("#").show();
-    $("#").hide();
+    $("#formulario_registro_foros_docente").show();
+  $("#formulario_programar_video_docente").hide();
+  $("#cartas_foros_docente").hide();
+  $("#cartas_cursos_asignados_docente").hide();
+  $("#cartas_asignaturas_asignados_docente").hide();
+$("#cartas_crear_video_docente").hide();
+  $("#cartas_ver_video_docente").hide();
 });
-
-$("#card-container").hide();
-$("#mostrar_foro").on("click", function() {
-    $("#card-container").show();
-    $("#").show();
-    $("#").hide();
-});
-$("#card-container_cursos").hide();
-$("#cursos_asignados_docente").on("click", function() {
-    $("#card-container_cursos").show();
-    $("#").show();
-    $("#").hide();
-});
-
-$("#card-container_asignaturas").hide();
-$("#asignaturas_asignados_docente").on("click", function() {
-    $("#card-container_asignaturas").show();
-    $("#").show();
-    $("#").hide();
-});
-$("#crear_videoconferencias").on("click", function() {
-    $("#").show();
-    $("#").show();
-    $("#").hide();
-});
+$("#formulario_programar_video_docente").hide();
 $("#programar_videoconferencias").on("click", function() {
-    $("#").hide();
-    $("#").show();
-    $("#").hide();
+  $("#formulario_programar_video_docente").show();
+$("#formulario_registro_foros_docente").hide();
+$("#cartas_foros_docente").hide();
+$("#cartas_cursos_asignados_docente").hide();
+$("#cartas_asignaturas_asignados_docente").hide();
+$("#cartas_crear_video_docente").hide();
+$("#cartas_ver_video_docente").hide();
 });
-$("#card-container_videoconferencias").hide();
+
+
+
+$("#cartas_foros_docente").hide();
+$("#mostrar_foro").on("click", function() {
+    $("#cartas_foros_docente").show();
+    $("#cartas_cursos_asignados_docente").hide();
+    $("#cartas_asignaturas_asignados_docente").hide();
+  $("#cartas_crear_video_docente").hide();
+    $("#cartas_ver_video_docente").hide();
+    $("#formulario_registro_foros_docente").hide();
+  $("#formulario_programar_video_docente").hide();
+
+});
+$("#cartas_cursos_asignados_docente").hide();
+$("#cursos_asignados_docente").on("click", function() {
+    $("#cartas_cursos_asignados_docente").show();
+    $("#cartas_foros_docente").hide();
+    $("#cartas_asignaturas_asignados_docente").hide();
+  $("#cartas_crear_video_docente").hide();
+    $("#cartas_ver_video_docente").hide();
+    $("#formulario_registro_foros_docente").hide();
+  $("#formulario_programar_video_docente").hide();
+
+});
+
+$("#cartas_asignaturas_asignados_docente").hide();
+$("#asignaturas_asignados_docente").on("click", function() {
+    $("#cartas_asignaturas_asignados_docente").show();
+    $("#cartas_foros_docente").hide();
+    $("#cartas_cursos_asignados_docente").hide();
+  $("#cartas_crear_video_docente").hide();
+    $("#cartas_ver_video_docente").hide();
+    $("#formulario_registro_foros_docente").hide();
+  $("#formulario_programar_video_docente").hide();
+
+});
+$("#cartas_crear_video_docente").hide();
+$("#crear_videoconferencias").on("click", function() {
+  $("#cartas_crear_video_docente").show();
+  $("#cartas_foros_docente").hide();
+  $("#cartas_cursos_asignados_docente").hide();
+  $("#cartas_asignaturas_asignados_docente").hide();
+  $("#cartas_ver_video_docente").hide();
+    $("#formulario_registro_foros_docente").hide();
+  $("#formulario_programar_video_docente").hide();
+
+});
+
+$("#cartas_ver_video_docente").hide();
 $("#mostrar_videoconferencias").on("click", function() {
-    $("#card-container_videoconferencias").show();
-    $("#").show();
-    $("#").hide();
+    $("#cartas_ver_video_docente").show();
+    $("#cartas_foros_docente").hide();
+    $("#cartas_cursos_asignados_docente").hide();
+    $("#cartas_asignaturas_asignados_docente").hide();
+  $("#cartas_crear_video_docente").hide();
+    $("#formulario_registro_foros_docente").hide();
+  $("#formulario_programar_video_docente").hide();
+
 });
 
 
@@ -59,88 +97,6 @@ $("#mostrar_videoconferencias").on("click", function() {
 
 
 
-
-// seccion de administradores}
-// listado y visualización de datos
-$("#cont_card").hide();
-$("#Mostrar_docentes").on("click", function() {
-    $("#listado_docentes").show();
-    $("#cont_card").show();
-    // todos los formularios ocultos
-    $("#formulario_registro_usuarios").hide();
-    $("#formulario_crear_curso").hide();
-    $("#formulario_crear_Asignatura").hide();
-    
-    // $("#").hide();
-});
-$("#listado_administradores").hide();
-$("#Mostrar_administradores").on("click", function() {
-    $("#listado_administradores").show();
-    // $("#").show();
-    $("#cont_card").show();
-    // todos los formularios ocultos
-    $("#formulario_registro_usuarios").hide();
-    $("#formulario_crear_curso").hide();
-    $("#formulario_crear_Asignatura").hide();
-    // $("#").hide();
-});
-$("#listado_cursos").hide();
-$("#Mostrar_cursos").on("click", function() {
-    $("#listado_cursos").show();
-    $("#cont_card").show();
-        // todos los formularios ocultos
-        $("#formulario_registro_usuarios").hide();
-        $("#formulario_crear_curso").hide();
-        $("#formulario_crear_Asignatura").hide();
-    // $("#").show();
-    // $("#").hide();
-});
-
-
-$("#listado_asignaturas").hide();
-$("#Mostrar_asignaturas").on("click", function() {
-    $("#listado_asignaturas").show();
-    // $("#").show();
-    $("#cont_card").show();
-        // todos los formularios ocultos
-        $("#formulario_registro_usuarios").hide();
-        $("#formulario_crear_curso").hide();
-        $("#formulario_crear_Asignatura").hide();
-    // $("#").hide();
-});
-
-// formularios administrador
-
-$("#formulario_crear_curso").hide();
-$("#registrar_cursos").on("click", function() {
-    $("#formulario_crear_curso").show();
-    $("#formulario_crear_Asignatura").hide();
-    $("#formulario_registro_usuarios").hide();
-    $("#formulario_crear_Asignatura").hide();
-
-    // se oculta las cartas
-    $("#cont_card").hide();
-});
-$("#formulario_registro_usuarios").hide();
-$("#Registrar_usuarios").on("click", function() {
-    $("#formulario_registro_usuarios").show();
-    $("#formulario_crear_curso").hide();
-    $("#formulario_crear_Asignatura").hide();
-
-    // se oculta las cartas
-    $("#cont_card").hide();
-});
-
-$("#formulario_crear_Asignatura").hide();
-$("#Registrar_Asignaturas").on("click", function() {
-    $("#formulario_crear_Asignatura").show();
-    $("#formulario_crear_curso").hide();
-    $("#formulario_registro_usuarios").hide();
-
-    // se oculta las cartas
-    $("#cont_card").hide();
-    // $("#").hide();
-});
 
 
 
@@ -274,82 +230,29 @@ $("#Registrar_Asignaturas").on("click", function() {
 
 
 
-// funcionamiento del modal visualizar, sección administrador
-// Get the modal
-var modal = document.getElementById("modalVisualizar");
 
-// Get the button that opens the modal
-var btn = document.getElementById("buttonVisualizar");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-// funcionamiento del modal editar, sección administrador
-// Get the modal
-var modal = document.getElementById("modalEditar");
-
-// Get the button that opens the modal
-var btn = document.getElementById("buttonEditar");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeModalEdit")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 
 
 // sweetalert de confirmación eliminacion
-$("#btn_eliminar_usuario_confirmar").on("click", function() {
-    Swal.fire({
-        title: "Desea eliminar el usuario?",
-        text: "Confirmar la eliminación",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Eliminar!"
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: "Eliminado!",
-            text: "Usuario eliminado.",
-            icon: "success"
-          });
-        }
-      });
-});
+// $("#btn_eliminar_usuario_confirmar").on("click", function() {
+//     Swal.fire({
+//         title: "Desea eliminar el usuario?",
+//         text: "Confirmar la eliminación",
+//         icon: "warning",
+//         showCancelButton: true,
+//         confirmButtonColor: "#3085d6",
+//         cancelButtonColor: "#d33",
+//         confirmButtonText: "Eliminar!"
+//       }).then((result) => {
+//         if (result.isConfirmed) {
+//           Swal.fire({
+//             title: "Eliminado!",
+//             text: "Usuario eliminado.",
+//             icon: "success"
+//           });
+//         }
+//       });
+// });
 
 });
