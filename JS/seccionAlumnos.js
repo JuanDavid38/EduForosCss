9943e1606cc418d40c306521919ee1b36c73d2d9
AndresP308 <preciadoandresfelipe71@gmail.com>
2024-02-28 22:59:12 -0500

    Cambios en formularios
    Cambios en formularios

--- JS/seccionAlumnos.js
+++ JS/seccionAlumnos.js
@@ -121,4 +121,32 @@ $("#btn_enviar_comentario_compañero").on("click", function() {
       }
     });
 });
+
+
+  // funcionamiento del modal mi perfil, sección alumno
+  // Get the modal
+  var modal = document.getElementById("modal_perfil_alumno");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn_visualizar_miPerfil");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeModalPerfil_alumno")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 });
